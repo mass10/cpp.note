ede708d66a11ccf421539a0f196409ed1d6d8083
mass10 <mass10.github@gmail.com>
2020-03-29 14:08:53 +0900

    wip

--- code/thread/pthread/sample0/main.cpp
+++ code/thread/pthread/sample0/main.cpp
@@ -11,33 +11,38 @@
 
 void* thread_func(void* param) {
 
-	printf("[TRACE] <thread_func()> thread started!\n");
+	printf("[TRACE] <thread_func()> $$$ start $$$\n");
 	int* status = (int*)param;
 	*status = 1;
-	// sleep(3);
-	printf("[TRACE] <thread_func()> thread exit!\n");
+
+	for (int i = 0; i < 10; i++) {
+		printf("[TRACE] <thread_func()> .\n");
+		usleep(100);
+	}
+	printf("[TRACE] <thread_func()> --- exit ---\n");
 	return NULL;
 }
 
 int main(int argc, char* argv[]) {
 
+	printf("[TRACE] <main()> ### start ###\n");
 	volatile int status = 0;
-	setlocale(LC_CTYPE, "ja_JP.UTF-8");
+	// setlocale(LC_CTYPE, "ja_JP.UTF-8");
 	pthread_t thread_id = 0;
 	// pthread_attr_t attr;
 	// pthread_attr_init(&attr);
-	printf("[TRACE] <thread_func()> creating thread...\n");
+	printf("[TRACE] <main()> creating thread...\n");
 	int error = pthread_create(&thread_id, NULL, thread_func, (void*)&status);
-	printf("[TRACE] %d\n", error);
 	if (error != 0) {
 		printf("[ERROR] cannot create a new thread...\n");
 		return 0;
 	}
+	printf("[TRACE] <main()> thread id: [%lu]\n", thread_id);
 	while (status == 0) {
-		printf(".\n");
-		sleep(0.1);
+		printf("[TRACE] <main()> .\n");
+		usleep(10);
 	}
 	pthread_join(thread_id, NULL);
-	printf("[TRACE] thread id: [%lu]\n", thread_id);
+	printf("[TRACE] <main()> --- end ---\n");
 	return 0;
 }
