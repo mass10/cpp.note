f3963c65956384dc1d614b41a373591f4ec08147
Irisawa Masaru <mass10.github@gmail.com>
2020-07-29 11:36:02 +0900

    wip

--- code/stl-windows/algorithm/algorithm.cpp
+++ code/stl-windows/algorithm/algorithm.cpp
@@ -7,6 +7,89 @@
 #include <string>
 #include <locale>
 
+// 超手抜き版。今だけ動けばいいとき。
+wchar_t to_upper(wchar_t c)
+{
+	if ('a' <= c && c <= 'z') return c - 32;
+	return c;
+}
+
+// 超手抜き版。今だけ動けばいいとき。
+wchar_t to_lower(wchar_t c)
+{
+	if ('A' <= c && c <= 'Z') return c + 32;
+	return c;
+}
+
+// 半角文字のみ変換
+wchar_t to_lower_正(wchar_t c)
+{
+	switch (c)
+	{
+	case 'A': return 'a';
+	case 'B': return 'b';
+	case 'C': return 'c';
+	case 'D': return 'd';
+	case 'E': return 'e';
+	case 'F': return 'f';
+	case 'G': return 'g';
+	case 'H': return 'h';
+	case 'I': return 'i';
+	case 'J': return 'j';
+	case 'K': return 'k';
+	case 'L': return 'l';
+	case 'M': return 'm';
+	case 'N': return 'n';
+	case 'O': return 'o';
+	case 'P': return 'p';
+	case 'Q': return 'q';
+	case 'R': return 'r';
+	case 'S': return 's';
+	case 'T': return 't';
+	case 'U': return 'u';
+	case 'V': return 'v';
+	case 'W': return 'w';
+	case 'X': return 'x';
+	case 'Y': return 'y';
+	case 'Z': return 'z';
+	}
+	return c;
+}
+
+wchar_t to_upper_正(wchar_t c)
+{
+	switch (c)
+	{
+	case 'a': return 'A';
+	case 'b': return 'B';
+	case 'c': return 'C';
+	case 'd': return 'D';
+	case 'e': return 'E';
+	case 'f': return 'F';
+	case 'g': return 'G';
+	case 'h': return 'H';
+	case 'i': return 'I';
+	case 'j': return 'J';
+	case 'k': return 'K';
+	case 'l': return 'L';
+	case 'm': return 'M';
+	case 'n': return 'N';
+	case 'o': return 'O';
+	case 'p': return 'P';
+	case 'q': return 'Q';
+	case 'r': return 'R';
+	case 's': return 'S';
+	case 't': return 'T';
+	case 'u': return 'U';
+	case 'v': return 'V';
+	case 'w': return 'W';
+	case 'x': return 'X';
+	case 'y': return 'Y';
+	case 'z': return 'Z';
+	}
+	return c;
+}
+
 std::wstring to_upper(const std::wstring& s)
 {
 	std::wstring result;
