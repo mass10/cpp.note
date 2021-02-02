9f179a759cc3170b432c91601518f4d64662b09d
Irisawa Masaru <irisawa.masaru@gmail.com>
2021-02-02 09:11:01 +0900

    wip

--- code/filesystem/examples/examples.cpp
+++ code/filesystem/examples/examples.cpp
@@ -4,8 +4,17 @@
 #include "stdafx.h"
 
 
+bool touch(const _TCHAR* path)
+{
+	FILE* file = NULL;
+	_tfopen_s(&file, path, _T("w+"));
+	if (file == NULL) return false;
+	fprintf(file, "hello!\n");
+	fclose(file);
+	return true;
+}
+
 int main()
 {
     return 0;
 }
-
