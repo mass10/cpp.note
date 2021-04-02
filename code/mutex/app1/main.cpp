b5a8bb2b5aae7e929999bd247a617de43d87745d
Irisawa Masaru <irisawa.masaru@gmail.com>
2021-04-03 00:28:53 +0900

    Update main.cpp

--- code/mutex/app1/main.cpp
+++ code/mutex/app1/main.cpp
@@ -7,13 +7,8 @@
 #include "..\common\util.h"
 #include "..\common\mutex.h"
 
-void _trace(const _TCHAR* message) {
-
-	const DWORD process_id = GetCurrentProcessId();
-	_tprintf(_T("%s [TRACE] [process %d] %s\n"), get_current_timestamp().c_str(), process_id, message);
-	fflush(stdout);
-}
-
+///////////////////////////////////////////////////////////////////////////////
+// アプリケーション本体
 class application {
 public:
 	void run();
@@ -24,10 +19,8 @@ void application::run() {
 	std::random_device generator;
 	const unsigned int task_id = generator();
 
-	{
-		mymutex lock;
-		_trace(_T("入店～"));
-	}
+	_trace(_T("$$$ start $$$"));
+	_trace(_T("(^ ◇ ^) 入店～"));
 
 	const unsigned long long start_tick = GetTickCount64();
 
@@ -36,31 +29,37 @@ void application::run() {
 		// 経過時間
 		const unsigned long long current_tick = GetTickCount64();
 		const unsigned long long elapsed_tick = current_tick - start_tick;
-		if (20000 <= elapsed_tick) {
+		if (600 * 3 <= elapsed_tick) {
 			break;
 		}
 
+		// ※※※ 連続した走査の排他ロック ※※※
 		mymutex lock;
-		_trace(_T("わたしの歌ーーーーー！"));
-		_trace(_T("(- ◇ -) アアア～♪ <1番"));
-		Sleep(900);
-		_trace(_T("(- ◇ -) アアア～♪ <2番"));
-		Sleep(900);
-		_trace(_T("つかれた...\n"));
+		_trace(_T("(^ ◇ ^) ★わたしの歌ーーーーー！"));
+		_trace(_T("(^ ◇ ^) アアア～♪ <1番"));
+		Sleep(200);
+		_trace(_T("(^ ◇ ^) アアア～♪ <2番"));
+		Sleep(200);
+		_trace(_T("(~ ◇ ~) つかれた...\n"));
 	}
 
-	{
-		mymutex lock;
-		_trace(_T("退出..."));
-	}
+	_trace(_T("(- ◇ -) 退出..."));
+	_trace(_T("--- end ---"));
 }
 
+///////////////////////////////////////////////////////////////////////////////
+// アプリケーションのエントリーポイント
 int main() {
 
+	// メモリ診断
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
+	// ロケール設定
 	_tsetlocale(LC_ALL, _T("Japanese"));
 
+	// アプリケーション実行
 	application app;
 	app.run();
+
+	return 0;
 }
