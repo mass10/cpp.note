31a728086a7883f521eff073985321df41aa9e03
Masaru Irisawa 入澤 賢 <irisawa.masaru@gmail.com>
2022-02-16 23:14:54 +0900

    Update main.cpp

--- code/console-text-color/main.cpp
+++ code/console-text-color/main.cpp
@@ -7,7 +7,9 @@
     #define ENABLE_VIRTUAL_TERMINAL_PROCESSING 4
 #endif
 
-
+//
+// 標準出力のカラーモード初期化
+//
 void InitializeConsoleColorIO()
 {
 	HANDLE stdOut = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -24,14 +26,17 @@ int _tmain(int argc, const _TCHAR* argv[])
 {
 	UNREFERENCED_PARAMETER(argc);
 
+	// メモリーリークをダンプする準備(デバッグモードのみ)
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
+	// 標準出力のカラーモード初期化
 	InitializeConsoleColorIO();
 
+	// 日本語を出力する準備
 	_tsetlocale(LC_ALL, _T("Japanese"));
 
 	_tprintf(_T("\x1b[32mHELLOOOOOOOOOOOOOO\x1b[39m\n"));
   
-  return 0;
+	return 0;
 }
 
